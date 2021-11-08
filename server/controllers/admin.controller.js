59f261cbbaf02e0f198d33b4897bb15e1c33346e
Rahul Nikam <codewithrahulnikam@gmail.com>
2021-11-08 12:42:31 -0800

    created add hospital page UI

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,5 +1,35 @@
+const mySql = require('mysql');
 
 
-exports.adminDashboard = (req,res) => {
-    res.render('dashboard.hbs', {title: 'Dashboard | HealthAura'});
+// DataBase Configuration and Connecting =>
+const pool = mySql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+
+
+exports.adminDashboard = (req, res) => {
+    res.render('dashboard.hbs', { title: 'Dashboard | HealthAura' });
+}
+
+
+exports.addNewHospitalPage = (req, res) => {
+    res.render('add-hospital.hbs', { title: "Add New Hospital | HealthAura" })
+}
+
+exports.addNewHospital = (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        else {
+            console.log('Database connected successfully');
+        }
+    })
 }
--- server/routers/admin.router.js
+++ server/routers/admin.router.js
@@ -5,5 +5,8 @@ const { checkLogin } = require('../../auth/check.login')
 const { isAdmin } = require('../../auth/check.isadmin')
 
 router.get('/', isAdmin, adminController.adminDashboard);
+router.get('/add-new-hospital', isAdmin, adminController.addNewHospitalPage);
+router.post('/add-new-hospital', isAdmin, adminController.addNewHospital);
+
 
 module.exports = router;
