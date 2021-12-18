cb65669531b71b17ee855455c8b89a6d5c0f6615
Rahul Nikam <codewithrahulnikam@gmail.com>
2021-12-17 22:55:23 -0800

    Day 17 completed, Conpleted send msg to admin and report bug system for client

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const mySql = require("mysql");
 const { verify } = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const nodeMailer = require('nodeMailer');
 
 // DataBase Configuration and Connecting =>
 const pool = mySql.createPool({
@@ -120,3 +121,121 @@ exports.updateUserCity = (req, res) => {
     }
   });
 };
+
+exports.sendMsgToAdmin = (req,res) => {
+  pool.getConnection((err,connection) => {
+    if(err) throw err;
+    const userToken = req.cookies.userToken;
+    if(userToken){
+    verify(userToken, process.env.SECRET_KEY, (err,decoded) => {
+      if(err){
+        res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:true, error: "Your Token has been expired, Try to login and than try again."})
+            console.log(false)
+      }
+      else{
+        const userEmail = decoded.result;
+        connection.query("select * from healthaura_users where userEmail = ?", [userEmail], (err, userData) => {
+          if(err){
+            res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:true, error: "Your Token has been expired, Try to login and than try again."})
+            console.log(false)
+          }
+          else{
+            const userEmail = decoded.result;
+            let transporter = nodeMailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: process.env.ADMIN_EMAIL,
+                pass: process.env.ADMIN_PASSWORD
+              }
+            })
+    
+            let template = {
+              from: `${userData[0].userName} <${userEmail}>`,
+              to: 'codewithrahulnikam@gmail.com',
+              subject: `${userData[0].userName} wants to contact you - HealthAura`,
+              text: `${userData[0].userName} saying - ${req.query.userMessageToAdmin}`
+            }
+
+            const sendEmailToAdmin = transporter.sendMail(template, (err, successEmailSent) => {
+              if(err){
+                res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:"Your Token has been expired, Try to login and than try again."})
+                console.log(false)
+              }
+              else{
+                res.render("message-page.hbs", {title: "Contact Admin | HealthAura", successEmailSent:"Your message has been sent successfully to Admin, He will contact you soon!"})
+                console.log(true)
+
+              }
+            })
+          }
+        })
+      }
+    })
+    }
+    else{
+      res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:"Your Token has been experied, Try to login and than try again."})
+
+    }
+  })
+}
+
+
+
+
+
+exports.reportBugEmail = (req,res) => {
+  pool.getConnection((err,connection) => {
+    if(err) throw err;
+    const userToken = req.cookies.userToken;
+    if(userToken){
+    verify(userToken, process.env.SECRET_KEY, (err,decoded) => {
+      if(err){
+        res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:true, error: "Your Token has been expired, Try to login and than try again."})
+            console.log(false)
+      }
+      else{
+        const userEmail = decoded.result;
+        connection.query("select * from healthaura_users where userEmail = ?", [userEmail], (err, userData) => {
+          if(err){
+            res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:true, error: "Your Token has been expired, Try to login and than try again."})
+            console.log(false)
+          }
+          else{
+            const userEmail = decoded.result;
+            let transporter = nodeMailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: process.env.ADMIN_EMAIL,
+                pass: process.env.ADMIN_PASSWORD
+              }
+            })
+    
+            let template = {
+              from: `${userData[0].userName} <${userEmail}>`,
+              to: 'codewithrahulnikam@gmail.com',
+              subject: `${userData[0].userName} Reported a bug - HealthAura`,
+              text: `${userData[0].userName} saying - ${req.query.reportBugMsg}`
+            }
+
+            const sendEmailToAdmin = transporter.sendMail(template, (err, successEmailSent) => {
+              if(err){
+                res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:"Your Token has been expired, Try to login and than try again."})
+                console.log(false)
+              }
+              else{
+                res.render("message-page.hbs", {title: "Contact Admin | HealthAura", successEmailSent:"Your Repoerted bug message has been successfully sent to HealthAura developers team, This will be fixed as soon as possible."})
+                console.log(true)
+
+              }
+            })
+          }
+        })
+      }
+    })
+    }
+    else{
+      res.render("message-page.hbs", {title: "Contact Admin | HealthAura", error:"Your Token has been expired, Try to login and than try again."})
+
+    }
+  })
+}
--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -6,6 +6,8 @@ const { checkLogin } = require('../../auth/check.login')
 
 router.get('/',tokenValidation, userController.userProfilePage)
 
-router.get('/update/userdata/:userName/city', tokenValidation, userController.updateUserCity)
+router.get('/update/userdata/:userName/city', tokenValidation, userController.updateUserCity);
+router.get("/contact-admin/send/message", tokenValidation, userController.sendMsgToAdmin)
+router.get("/report/bug", tokenValidation, userController.reportBugEmail)
 
 module.exports = router;
